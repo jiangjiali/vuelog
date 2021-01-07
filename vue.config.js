aeb7c4e19d9679d22a2d07c7d88dda10380f5614
蒋佳李 <1036226474@qq.com>
2021-01-07 11:33:33 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,18 @@ const prodConfig = {
   externals: {
     database: 'Vuelog.database'
   },
+  // 警告 webpack 的性能提示
+  performance: {
+    hints: 'warning',
+    // 入口起点的最大体积
+    maxEntrypointSize: 3145728,
+    // 生成文件的最大体积
+    maxAssetSize: 1572864,
+    // 只给出 js 文件的性能提示
+    assetFilter: function (assetFilename) {
+      return assetFilename.endsWith('.js')
+    }
+  },
   // plugins: [
   //   new WebpackCdnPlugin({
   //     modules: [
